4742c6291274c1650a2ebf359d5af787cc980ee0
Tetiana Perederii <lapshun.tatiana@gmail.com>
2025-11-26 17:42:50 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
+#include "MyStack.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    myStack::Stack<int> numbers;
+
+    numbers.showStack();
+    cout << "======================\n";
+    numbers.push_back(6);
+    numbers.push_back(-2);
+    numbers.push_back(1);
+    numbers.push_back(8);
+    numbers.showStack();
+    cout << "======================\n";
+
+    //Розвантаження стеку
+    while (!numbers.isEmpty()) {
+        cout << "TOP: " << numbers.top() << endl;
+        numbers.showStack();
+        cout << endl;
+        numbers.pop_back();
     }
+    numbers.showStack();
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
